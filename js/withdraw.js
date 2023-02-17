361574dcabaac39abc119c880ac0509ee272554a
Aditta Chakraborty <adittacse@gmail.com>
2023-02-17 16:44:42 +0600

    validation added in withdraw

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,22 +4,34 @@ document.getElementById("btn-withdraw").addEventListener("click", function() {
     const withdrawField = document.getElementById("withdraw-field");
     const newWithdrawAmount = parseFloat(withdrawField.value);
 
-    // step 3: get previous withdraw total
+    // step 3: clear the deposit field
+    withdrawField.value = "";
+
+    // is type is "text"
+    // if (isNan(newWithdrawAmount)) {
+    //     alert("Please enter a valid number");
+    //     return;
+    // }
+
+    // step 4: get previous withdraw total
     const previousWithdrawElement = document.getElementById("withdraw-total");
     const previousWithdrawTotal = parseFloat(previousWithdrawElement.innerText);
-
-    // step 4: set withdraw amount
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    previousWithdrawElement.innerText = currentWithdrawTotal;
 
     // step 5: get the current total balance
     const balanceTotalElement = document.getElementById("balance-total");
     const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
 
-    // step 6: calculating final balance
+    // step 7: validation
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert("Not enough money");
+        return;
+    }
+
+    // step 8: set withdraw amount
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    previousWithdrawElement.innerText = currentWithdrawTotal;
+
+    // step 9: calculating final balance
     const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = currentBalanceTotal;
-
-    // step 7: clear the deposit field
-    withdrawField.value = "";
 });
